44e29b74306796a6dc4b656a65c97a5d660a7fde
brentjm <brent_maranzano@yahoo.com>
2018-07-17 13:59:22 -0400

    Added method to get filled positions in last N days

--- program.py
+++ program.py
@@ -4,6 +4,19 @@ Example to retrieve data from TD Ameritrade API
 import urllib.request
 from datetime import datetime, timedelta
 from pdb import set_trace
+import json
+
+
+def dump_message(fn):
+    """Dump the message to file.
+    Useful for debugging the message parser.
+    """
+    def wrapper(self, url):
+        fn(self, url)
+        with open("message.txt", mode='w') as file_obj:
+            file_obj.writelines(json.dumps(self.message))
+
+    return  wrapper
 
 class TDAmeritrade:
     """Class to create http urls to conform to the TDAmeritrade API.
@@ -34,25 +47,25 @@ class TDAmeritrade:
         oauth_hash = oauth_hash.rstrip("\n")
         return oauth_hash
 
-    def __set_request(self, url):
-        """Set the request based on the base url plus the supplied url.
+    @dump_message
+    def _send_request(self, url):
+        """Send the request based on the base url plus the supplied url.
         """
         base_url = "https://api.tdameritrade.com/v1/accounts/{}".format(self.account_no)
         url = base_url + url
         print(url)
         request = urllib.request.Request(url)
         request.add_header("Authorization", "Bearer {}".format(self.oauth_hash).encode("utf-8"))
-        return request
+        response = urllib.request.urlopen(request)
+        self.message = json.loads(response.read().decode("utf-8"))
 
-    def get_account_info(self, fields="positions"):
+    def _account_info(self, fields="positions,orders"):
         """Get account information.
         """
         url = "?fields={}".format(fields)
-        request = self.__set_request(url)
-        response = urllib.request.urlopen(request)
-        self.message = response.read()
+        self._send_request(url)
 
-    def get_orders(self, max_results=50, from_date=None, to_date=None, order_status=None):
+    def _orders(self, max_results=100, from_date=None, to_date=None, order_status="FILLED"):
         """Get order information.
         """
         if to_date is None:
@@ -65,17 +78,27 @@ class TDAmeritrade:
         else:
             url = "/orders?maxResults={}&fromEnteredTime={}&toEnteredTime={}&status={}"\
                 .format(max_results, from_date, to_date, order_status)
-        request = self.__set_request(url)
-        response = urllib.request.urlopen(request)
-        self.message = response.read()
+        self._send_request(url)
+
+    def get_month_filled(self):
+        """Get the filled positions within the last 30 days.
+        """
+        to_date = datetime.strftime(datetime.today(), format="%Y-%m-%d")
+        from_date = datetime.strftime(datetime.today() - timedelta(35), format="%Y-%m-%d")
+        self._orders(max_results=100, from_date=from_date, to_date=to_date, order_status="FILLED")
+        orders = self.message
+        symbols = list()
+        for order in orders:
+            symbols.append(order["orderLegCollection"][0]["instrument"]["symbol"])
+        return symbols
 
 def test():
     """Run tests.
     """
     app = TDAmeritrade(filename_account="account_no.txt", filename_oauth="oAuth_hash.txt")
-    app.get_account_info()
-    app.get_orders()
-    print(app.message)
+    #app._account_info()
+    #app._orders()
+    print(app.get_month_filled())
 
 if __name__ == "__main__":
     test()
